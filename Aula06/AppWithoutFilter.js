7d938f9a6cf7b03038eda35662e4e99995a295e5
Rodrigo Escobar <rodescobar@gmail.com>
2025-02-27 17:33:28 -0300

    Aula06

---
+++ Aula06/AppWithoutFilter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import data from './data.json';
+
+function AppWithoutFilter() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default AppWithoutFilter;
